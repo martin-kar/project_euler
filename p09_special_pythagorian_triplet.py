2041950ca550a67a0dccdb5ab3fd6d45b38be52c
Martin Karlsson <martin@builtrobotics.com>
2020-04-08 11:51:56 -0700

    Fix code style

--- p09_special_pythagorian_triplet.py
+++ p09_special_pythagorian_triplet.py
@@ -1,32 +1,38 @@
-'''
+"""
 Special Pythagorean triplet
 Problem 9
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
-'''
+"""
 
-# Takes integers a,b,c. Returns boolean of whether a,b,c is Pythagorean 
-# triplet:
-def is_pyth_triplet(a,b,c):
-	return (a**2 + b**2 == c**2) & (a<b) & (b<c)
-
-abc = 0
+REQUIRED_SUM_OF_ABC = 1000
 
 
-found = False # We know there is exactly 1 triplet. 
-# found indicates whether the triplet is found, so we can stop 
-# searching.
+def is_pythagorean_triplet(a, b, c):
+    return (a ** 2 + b ** 2 == c ** 2) and (a < b) and (b < c)
 
-# Run the search:
-for a in range(1,1000):
-	print(a)
-	if found:
-		break
-	for b in range(a+1,1000):
-		for c in range(b+1,1000):
-			if is_pyth_triplet(a,b,c) & ( (a+b+c)==1000):
-				print (a,b,c)
-				abc = a*b*c
-				found = True
-print(abc)
+
+def product_of_abc_found(product_of_abc):
+    return product_of_abc is not None
+
+
+def get_product_of_abc():
+    product_of_abc = None
+    for a in range(1, REQUIRED_SUM_OF_ABC):
+        if product_of_abc_found(product_of_abc):
+            break
+        for b in range(a + 1, REQUIRED_SUM_OF_ABC):
+            c = REQUIRED_SUM_OF_ABC - a - b
+            if is_pythagorean_triplet(a, b, c):
+                product_of_abc = a * b * c
+    return product_of_abc
+
+
+def main():
+    product_of_abc = get_product_of_abc()
+    print(product_of_abc)
+
+
+if __name__ == '__main__':
+    main()
--- p10_summation_of_primes.py
+++ p10_summation_of_primes.py
@@ -1,29 +1,29 @@
-'''
+"""
 Summation of primes
 Problem 10
 
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
-'''
+"""
 
-lower_primes = [] # Stores primes found so far.
+import math
 
-# Checks whether nbr is prime, using previously found primes:
-def isprime(nbr, lower_primes):
-	for prime in lower_primes:
-		if nbr%prime == 0:
-			return False
-	return True
+UPPER_LIMIT = 2 * 10 ** 6
 
-upper_limit = 2*10**6
-sum = 0
 
-for nbr in range(2,upper_limit):
-	print(nbr)
-	# If nbr is a prime, we add it to the sum and 
-	# save it in lower_primes:
-	if isprime(nbr, lower_primes):
-		sum += nbr
-		lower_primes.extend([nbr])
+def is_prime(candidate_nbr, lower_primes):
+    for prime in lower_primes:
+        if prime > math.sqrt(candidate_nbr):
+            break
+        if candidate_nbr % prime == 0:
+            return False
+    return True
 
-print(sum)
+
+sum_of_primes = 2
+saved_lower_primes = [2]
+for nbr in range(3, UPPER_LIMIT, 2):
+    if is_prime(nbr, saved_lower_primes):
+        sum_of_primes += nbr
+        saved_lower_primes.append(nbr)
+print(sum_of_primes)
