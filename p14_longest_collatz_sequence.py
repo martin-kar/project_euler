04aa5960ee002ca32d55c255ac7092c7dbe8d03d
Martin Karlsson <martin@builtrobotics.com>
2020-04-09 15:24:32 -0700

    Fix code style

--- p14_longest_collatz_sequence.py
+++ p14_longest_collatz_sequence.py
@@ -1,28 +1,43 @@
-'''
+"""
 Longest Collatz sequence
 Problem 14
-Which starting number, under one million, produces the longest 
+Which starting number, under one million, produces the longest
 Collatz sequence?
-'''
+"""
 
-longest_length = 1 # Stores record so far.
-record_number = 1 # Corresponding starting number for record.
+MAX_START_NUMBER = 10 ** 6
 
-# Search until limit:
-for i in range(2,10**6):
-	chain_length = 1
-	n = i
-	print(i)
-	while n > 1:
-		if n%2==0:
-			n /= 2
-		else:
-			n = 3*n + 1
-		chain_length += 1
-		# Make some updates if we have a new record: 
-		if chain_length > longest_length:
-			longest_length = chain_length
-			record_number = i
-		
-print(record_number)
-	
+
+def get_record_number():
+    record_length = 1
+    record_number = 1
+    for starting_number in range(2, MAX_START_NUMBER):
+        sequence_length = get_sequence_length(starting_number)
+        if is_new_record(sequence_length, record_length):
+            record_length = sequence_length
+            record_number = starting_number
+    return record_number
+
+
+def get_sequence_length(starting_number):
+    sequence_length = 1
+    n = starting_number
+    while n > 1:
+        n = get_collatz_update(n)
+        sequence_length += 1
+    return sequence_length
+
+
+def is_new_record(sequence_length, record_length):
+    return sequence_length > record_length
+
+
+def get_collatz_update(n):
+    if n % 2 == 0:
+        n_next = n / 2
+    else:
+        n_next = 3 * n + 1
+    return n_next
+
+
+print(get_record_number())
