42f3de7f1f42a47341f0e4b6db799a4bbb234470
Martin Karlsson <martin@builtrobotics.com>
2020-04-09 12:10:37 -0700

    Make algorithm faster and cleaner

--- p12_highly_divisible_triangular_numbers.py
+++ p12_highly_divisible_triangular_numbers.py
@@ -1,60 +1,69 @@
-'''
+"""
 Highly divisible triangular number
 Problem 12
 What is the value of the first triangle number to have over five hundred divisors?
-'''
+"""
 
 import numpy as np
 
-# Returns multiplicities, which is the multiplicity of each prime in 
-# primes given the integer nbr:
-def get_multiplicities(nbr, primes):
-	multiplicities = np.array([])
-	for prime in primes:
-		multiplicity = 0
-		if prime > nbr:
-			break
-		while nbr%prime == 0:
-			multiplicity += 1
-			nbr /= prime
-		if multiplicity > 0:
-			multiplicities = np.append(multiplicities,multiplicity)
-	return multiplicities
 
-# Returns the total number of divisors of integer nbr:
-def nbr_divisors(nbr, primes):
-	multiplicities = get_multiplicities(nbr, primes)
-	return np.prod(multiplicities+1)
-	
-# Prepare a list of primes (named primes) up to max_nbr.
-def prepare_primes(max_nbr):
-	primes = [2]	
-	nbr = 2
-	while len(primes) < max_nbr:
-		nbr_is_prime = True
-		for prime in primes:
-			if nbr%prime == 0:
-				nbr_is_prime = False
-		if nbr_is_prime:
-			primes.extend([nbr])
-		nbr += 1
-	return primes
+REQUIRED_NUMBER_OF_DIVISORS = 500
 
-# Given integer max_nbr_of_divisors, returns number with that 
-# amount of divisors.
-def get_divisible_nbr(max_nbr_of_divisors):
-	primes = prepare_primes(max_nbr_of_divisors)
-	natural_nbr = 1
-	triangle_nbr = 1
-	largest = 0
-	while True:
-		nbr_of_divisors = nbr_divisors(triangle_nbr, primes)
-		if nbr_of_divisors > largest:
-			largest = nbr_of_divisors
-			print(largest)
-		if nbr_of_divisors > max_nbr_of_divisors:
-			return triangle_nbr
-		natural_nbr += 1
-		triangle_nbr += natural_nbr
 
-print(get_divisible_nbr(500))
+def get_non_zero_multiplicities(nbr, primes):
+    multiplicities = []
+    for prime in primes:
+        multiplicity = get_multiplicity(prime, nbr)
+        if multiplicity > 0:
+            multiplicities.append(multiplicity)
+    return multiplicities
+
+
+def get_multiplicity(factor, nbr_to_factorize):
+    multiplicity = 0
+    while nbr_to_factorize % factor == 0 and nbr_to_factorize > 0:
+        multiplicity += 1
+        nbr_to_factorize /= factor
+    return multiplicity
+
+
+def get_nbr_of_divisors(nbr, primes):
+    multiplicities = get_non_zero_multiplicities(nbr, primes)
+    return np.prod(np.array(multiplicities) + 1)
+
+
+def get_primes():
+    primes = [2, 3]
+    nbr = 3
+    while len(primes) < REQUIRED_NUMBER_OF_DIVISORS:
+        nbr += 2
+        if is_prime(nbr, primes):
+            primes.append(nbr)
+    return primes
+
+
+def is_prime(nbr, saved_primes):
+    for prime in saved_primes:
+        if prime ** 2 > nbr:
+            return True
+        if nbr % prime == 0:
+            return False
+    return True
+
+
+def get_divisible_nbr():
+    primes = get_primes()
+    natural_nbr = 1
+    triangle_nbr = 1
+    largest = 0
+    while True:
+        nbr_of_divisors = get_nbr_of_divisors(triangle_nbr, primes)
+        if nbr_of_divisors > largest:
+            largest = nbr_of_divisors
+        if nbr_of_divisors > REQUIRED_NUMBER_OF_DIVISORS:
+            return triangle_nbr
+        natural_nbr += 1
+        triangle_nbr += natural_nbr
+
+
+print(get_divisible_nbr())
