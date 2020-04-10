2f7dbcec0027c5cf90775f16f476faf3bf3ef906
Martin Karlsson <martin@builtrobotics.com>
2020-04-09 21:58:51 -0700

    Fix code style

--- p15_lattice_paths.py
+++ p15_lattice_paths.py
@@ -1,16 +1,15 @@
-'''
+"""
 Lattice paths
 Problem 15
-'''
 
-#Solution:
-'''
-We don't write a program for this, just realize that there are 
+First we realize that there are
 40!/(20!*20!) ways to order two different directions of 20 each, in
 a sequence with total length 40.
+"""
 
-Answer: 137846528820
-'''
-# This short program prints the answer we already know :) 
-print(137846528820)
+import math
 
+GRID_SIZE = 20
+
+nbr_of_combinations = math.factorial(2 * GRID_SIZE) / math.factorial(GRID_SIZE) ** 2
+print(nbr_of_combinations)
--- p16_power_digit_sum.py
+++ p16_power_digit_sum.py
@@ -1,13 +1,7 @@
-'''
+"""
 Power digit sum
 Problem 16
 What is the sum of the digits of the number 2 to the power of 1000?
-'''
+"""
 
-number = str(2**1000) # The number converted to a string.
-sum = 0
-for ch in number: # Iterate over string:
-	sum += int(ch) # Add each digit to sum.
-
-print(sum)
-
+print(sum([int(c) for c in str(2**1000)]))
